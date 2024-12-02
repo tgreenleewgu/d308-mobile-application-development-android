16efd104fb6ca73e1bfb72cf01dba53369812b77
Travis Greenlee <tgr1196@wgu.edu>
2024-12-01 20:24:04 -0500

    Webinar 1 break point

--- Vacations2/app/src/main/java/com/example/vacations/UI/VacationInformation.java
+++ Vacations2/app/src/main/java/com/example/vacations/UI/VacationInformation.java
@@ -1,4 +1,4 @@
-package com.example.vacations;
+package com.example.vacations.UI;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.vacations.R;
+
+public class VacationInformation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_vacation_information);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ Vacations2/app/src/main/java/com/example/vacations/UI/Vacation_List.java
@@ -0,0 +1,59 @@
+package com.example.vacations.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.vacations.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class Vacation_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_vacation_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Vacation_List.this, VacationInformation.class);
+                startActivity(intent);
+            }
+        });
+
+        System.out.println(getIntent().getStringExtra("vacation"));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       if(item.getItemId()==R.id.add_vacation){
+           Toast.makeText(Vacation_List.this, "Add Vacation", Toast.LENGTH_LONG).show();
+           return true;
+
+
+}
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
